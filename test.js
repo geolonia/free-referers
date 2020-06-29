f6cfb68a7e2a6ddfd18306691bf312e17d2524f0
kamataryo <kamataryo@users.noreply.github.com>
2020-06-29 11:31:57 +0900

    Some fix

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 const fs = require('fs')
 const stringifiedJSON = fs.readFileSync(__dirname + '/referers.json', 'utf-8')
-JSON.parse(stringifiedJSON)
+
+const referers = JSON.parse(stringifiedJSON)
+
+if(!Array.isArray(referers)) {
+  throw new Error('The referers should be a Array.')
+}
+
+if(referers.some(referer => typeof referer !== 'string')) {
+  throw new Error('Each referer should be a string.')
+}
